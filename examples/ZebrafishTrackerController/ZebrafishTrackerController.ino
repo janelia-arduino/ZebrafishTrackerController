856095fddbcd46aba82204f9756f2f2d55317ea1
Peter Polidoro <peterpolidoro@gmail.com>
2018-03-28 17:06:00 -0400

    Removed unnecessary header includes from ino files.

--- examples/ZebrafishTrackerController/ZebrafishTrackerController.ino
+++ examples/ZebrafishTrackerController/ZebrafishTrackerController.ino
@@ -1,25 +1,3 @@
-#include <Flash.h>
-#include <EEPROM.h>
-#include <Streaming.h>
-#include <ArduinoJson.h>
-#include <JsonSanitizer.h>
-#include <JsonStream.h>
-#include <Array.h>
-#include <Vector.h>
-#include <MemoryFree.h>
-#include <ConstantVariable.h>
-#include <SavedVariable.h>
-#include <Functor.h>
-#include <IndexedContainer.h>
-#include <FunctorCallbacks.h>
-#include <Watchdog.h>
-
-#include <ModularClient.h>
-
-#include <ModularServer.h>
-#include <ModularDeviceBase.h>
-#include <StepDirController.h>
-#include <StageController.h>
 #include <ZebrafishTrackerController.h>
 
 
