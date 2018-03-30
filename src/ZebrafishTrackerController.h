3e0c4961b30fa88903286370c1c35fac5369a696
Peter Polidoro <peterpolidoro@gmail.com>
2018-03-30 14:12:32 -0400

    Removed unnecessary header includes.

--- src/ZebrafishTrackerController.h
+++ src/ZebrafishTrackerController.h
@@ -7,23 +7,12 @@
 // ----------------------------------------------------------------------------
 #ifndef ZEBRAFISH_TRACKER_CONTROLLER_H
 #define ZEBRAFISH_TRACKER_CONTROLLER_H
-#include <Flash.h>
-#include <EEPROM.h>
-#include <Streaming.h>
 #include <ArduinoJson.h>
-#include <JsonSanitizer.h>
 #include <JsonStream.h>
 #include <Array.h>
 #include <Vector.h>
-#include <MemoryFree.h>
 #include <ConstantVariable.h>
-#include <SavedVariable.h>
 #include <Functor.h>
-#include <IndexedContainer.h>
-#include <FunctorCallbacks.h>
-#include <Watchdog.h>
-
-#include <ModularClient.h>
 
 #include <ModularServer.h>
 #include <ModularDeviceBase.h>
