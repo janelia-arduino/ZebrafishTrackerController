455b5c790ac0bac8d50ceb81418337cdd3a12901
Peter Polidoro <peterpolidoro@gmail.com>
2020-09-04 17:01:20 -0400

    Update to work with ArduinoJson 6

--- src/ZebrafishTrackerController/ZebrafishTrackerController.cpp
+++ src/ZebrafishTrackerController/ZebrafishTrackerController.cpp
@@ -94,8 +94,8 @@ void ZebrafishTrackerController::setup()
 // floating-point number (float, double)
 // bool
 // const char *
-// ArduinoJson::JsonArray *
-// ArduinoJson::JsonObject *
+// ArduinoJson::JsonArray
+// ArduinoJson::JsonObject
 // const ConstantString *
 //
 // For more info read about ArduinoJson parsing https://github.com/janelia-arduino/ArduinoJson
